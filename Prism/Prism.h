5dd9ed9221df294e0e8036176c6f538785266a76
guochunmao <guochunmao@100tal.com>
2021-05-21 14:40:20 +0800

    xcode project

---
+++ Prism/Prism.h
@@ -0,0 +1,19 @@
+//
+//  Prism.h
+//  Prism
+//
+//  Created by 郭春茂 on 2021/5/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Prism.
+FOUNDATION_EXPORT double PrismVersionNumber;
+
+//! Project version string for Prism.
+FOUNDATION_EXPORT const unsigned char PrismVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Prism/PublicHeader.h>
+
+
+//#include "Prism-J2Objc.h"
